3e8fd6cb11d92ef6cb85384aeabe1c50fcdc09a1
gamsathvani <134626131+gamsathvani@users.noreply.github.com>
2023-06-10 15:17:54 +0530

    Add files via upload

---
+++ cir 2.py
@@ -0,0 +1,25 @@
+turtle.pensize(40)
+def draw_circle(size,angle,shift):
+    turtle.bgcolor(next(colors))
+import turtle
+from itertools import circle
+
+colors=cycle(['red','orange','yellow','green','blue','purple'])
+
+def draw_circle(size,angle,shift,shape):
+    turtle.pencolor(next(colors))
+    next_shape=' '
+    if shape=='circle':
+        next_shape='square'
+       else shape=='square':
+            for i in range(4):
+                turtle.forward(size=2)
+                turtle.left(90)
+                next_shape='circle'
+                turtle.right(angle)                
+                turtle.forward(shift)
+                draw_shape(size +5,angle+1,shift+1,next_shape)
+    turtle.bgcolor('black')
+    turtle.speed('fast')
+    turtle.pensize(4)
+    draw_shape(30,0,1,'circle')
